17c4556ffac954da9a7e4aceabaa32fb8783dd8d
Ubuntu <ubuntu@ip-172-31-21-7.ap-northeast-1.compute.internal>
2014-03-22 13:46:37 +0000

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CapTest2::Application.config.secret_key_base = 'de81f7952376f34008affc54a211d658e305e76228209aa2bd448b7e668558ace564993054ce8cf712482d727cc6107bb5fe6a958bc00eacca1f4b9e8c88a54f'
